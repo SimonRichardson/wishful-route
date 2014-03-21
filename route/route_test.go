3dddda37e863f009418b5ad177ae43bf3d2047ad
Simon Richardson <stickupkid@gmail.com>
2014-03-21 09:42:14 +0000

    Adding tests for the route

--- route/route_test.go
+++ route/route_test.go
@@ -2,13 +2,84 @@ package route
 
 import (
 	"testing"
+	"testing/quick"
 	. "github.com/SimonRichardson/wishful/useful"
 	. "github.com/SimonRichardson/wishful/wishful"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestSomething(t *testing.T) {
-	fallback := ConstantNoArgs("Hello")
-	res := Route(fallback, make([]func(x AnyVal) Option, 0, 0))
-	assert.Equal(t, res(nil), "Hello")
+func Test_CallsFallbackIfEmptyList(t *testing.T) {
+	f := func(x string) string {
+		fallback := ConstantNoArgs(x)
+		res := Route(fallback, make([]func(x AnyVal) Option, 0, 0))
+		return res(nil).(string)
+	}
+	g := func(x string) string {
+		return x
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_CallsFirstMatch(t *testing.T) {
+	f := func(x string, y string, z string) string {
+		fallback := ConstantNoArgs(x)
+		routes := []func(x AnyVal) Option{
+			func(a AnyVal) Option {
+				return NewSome(y)
+			},
+		}
+		res := Route(fallback, routes)
+		return res(z).(string)
+	}
+	g := func(x string, y string, z string) string {
+		return y
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_CallsFirstMatchWhenMultiple(t *testing.T) {
+	f := func(x string, y string, z string) string {
+		fallback := ConstantNoArgs(x)
+		routes := []func(x AnyVal) Option{
+			func(a AnyVal) Option {
+				return NewNone()
+			},
+			func(a AnyVal) Option {
+				return NewSome(y)
+			},
+		}
+		res := Route(fallback, routes)
+		return res(z).(string)
+	}
+	g := func(x string, y string, z string) string {
+		return y
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_CallsFallbackIfNoMatch(t *testing.T) {
+	f := func(x string, y string, z string) string {
+		fallback := ConstantNoArgs(x)
+		routes := []func(x AnyVal) Option{
+			func(a AnyVal) Option {
+				return NewNone()
+			},
+			func(a AnyVal) Option {
+				return NewNone()
+			},
+		}
+		res := Route(fallback, routes)
+		return res(z).(string)
+	}
+	g := func(x string, y string, z string) string {
+		return x
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
 }
