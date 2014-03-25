c07dc8de55b940c88afd3bc54da43e35e0f6ddc7
Simon Richardson <simon@ustwo.co.uk>
2014-03-25 22:06:23 +0000

    Finished adding body tests

--- route/body_test.go
+++ route/body_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 	"testing"
 	"testing/quick"
@@ -23,6 +24,13 @@ func NewPartial(x string) *Partial {
 func (x *Partial) Show() (string, string) {
 	a, _ := json.Marshal(x)
 	return string(a), fmt.Sprintf("%d", len(a))
+}
+
+func values(x string) (string, string) {
+	v := url.Values{}
+	v.Set("xx", x)
+	e := v.Encode()
+	return e, fmt.Sprintf("%d", len(e))
 }
 
 func request(body string, length string) *http.Request {
@@ -109,12 +117,83 @@ func Test_JsonShouldReturnPartial(t *testing.T) {
 	}
 }
 
-func Test_JsonShouldReturnErrorIfInvalidPartial(t *testing.T) {
+func Test_JsonShouldReturnErrorIfInvalidBody(t *testing.T) {
 	f := func(x string) bool {
 		a, b := NewPartial(x).Show()
 
 		var partial Partial
 		promise := Json(&partial, request(a[0:len(a)-2], b)).Run.(Promise)
+		return promise.Fork(func(x AnyVal) AnyVal {
+			return x.(Either).Fold(Constant(true), Constant(false))
+		}).(bool)
+	}
+	g := func(x string) bool {
+		return true
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_JsonShouldReturnErrorIfInvalidBodyWhichShouldThrowParseError(t *testing.T) {
+	f := func(x string) bool {
+		a := `{"asd`
+		b := fmt.Sprintf("%d", len(a))
+
+		var partial Partial
+		promise := Json(&partial, request(a, b)).Run.(Promise)
+		return promise.Fork(func(x AnyVal) AnyVal {
+			return x.(Either).Fold(Constant(true), Constant(false))
+		}).(bool)
+	}
+	g := func(x string) bool {
+		return true
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_QueryShouldReturnValues(t *testing.T) {
+	f := func(x string) url.Values {
+		promise := Query(request(values(x))).Run.(Promise)
+		return promise.Fork(func(x AnyVal) AnyVal {
+			return x.(Either).Fold(Identity, Identity)
+		}).(url.Values)
+	}
+	g := func(x string) url.Values {
+		v := url.Values{}
+		v.Set("xx", x)
+		return v
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_QueryShouldReturnErrorIfInvalidBody(t *testing.T) {
+	f := func(x string) bool {
+		a, b := values(x)
+
+		promise := Query(request(a[0:len(a)-2], b)).Run.(Promise)
+		return promise.Fork(func(x AnyVal) AnyVal {
+			return x.(Either).Fold(Constant(true), Constant(false))
+		}).(bool)
+	}
+	g := func(x string) bool {
+		return true
+	}
+	if err := quick.CheckEqual(f, g, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_QueryShouldReturnErrorIfInvalidBodyWhichShouldThrowParseError(t *testing.T) {
+	f := func(x string) bool {
+		a := "%as=hello"
+		b := fmt.Sprintf("%d", len(a))
+
+		promise := Query(request(a, b)).Run.(Promise)
 		return promise.Fork(func(x AnyVal) AnyVal {
 			return x.(Either).Fold(Constant(true), Constant(false))
 		}).(bool)
