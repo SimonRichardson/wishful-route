6ff79bfc72c7a87a355a3b33e47a7fc95854e993
Simon Richardson <stickupkid@gmail.com>
2014-03-31 11:24:48 +0100

    Use IO monads when implementing servers

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ const port string = "127.0.0.1:8080"
 func main() {
 	fmt.Println("Booting...")
 
-	Serve(Listen(port, Route(
+	server := Serve(Listen(port, Route(
 		func() Promise {
 			return NotFound("Nope!")
 		},
@@ -33,7 +33,9 @@ func main() {
 				}),
 			),
 		},
-	))).(Either).Fold(
+	))).UnsafePerform()
+
+	server.(Either).Fold(
 		func(x AnyVal) AnyVal {
 			fmt.Println("Failed to listen:", x)
 			return x
--- route/server.go
+++ route/server.go
@@ -10,28 +10,36 @@ type Server struct {
 	server *http.Server
 }
 
-func Listen(address string, route func(x *Request) Promise) *Server {
-	return &Server{
-		server: &http.Server{
-			Addr:    address,
-			Handler: http.HandlerFunc(handle(route)),
-		},
-	}
+func Listen(address string, route func(x *Request) Promise) IO {
+	return NewIO(func() AnyVal {
+		return &Server{
+			server: &http.Server{
+				Addr:    address,
+				Handler: http.HandlerFunc(handle(route)),
+			},
+		}
+	})
 }
 
-func Serve(s *Server) Either {
-	if err := s.server.ListenAndServe(); err != nil {
-		return NewLeft(err)
-	}
-	return NewRight(s)
+func Serve(s IO) IO {
+	return s.Map(func(x AnyVal) AnyVal {
+		a := x.(*Server)
+		if err := a.server.ListenAndServe(); err != nil {
+			return NewLeft(err)
+		}
+		return NewRight(a)
+	}).(IO)
 }
 
 func Handle(route func(x *Request) Promise) IO {
-	return IO{}.Of(http.HandlerFunc(handle(route))).(IO)
+	return NewIO(func() AnyVal {
+		return http.HandlerFunc(handle(route))
+	})
 }
 
 func handle(route func(x *Request) Promise) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// I wonder if we should pass a response (monad writer)
 		route(NewRequest(r)).Fork(func(x AnyVal) AnyVal {
 			result := x.(Result)
 			header := w.Header()
